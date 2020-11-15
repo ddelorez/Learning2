2edfebedbdf3ba3715702ca722c5ae304be6f0c3
ddelorez <ddelorez@gmail.com>
2020-11-15 16:41:49 -0600

    expanding on OOP

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 #OOP
 
 class PlayerCharacter:
-    def __init__(self, name):
+    def __init__(self, name, age):
         self.name = name
-        
+        self.age = age
 
     def run(self):
         print("run")
 
 
-player1 = PlayerCharacter("Dave")
-player2 = PlayerCharacter("Lily")
+player1 = PlayerCharacter("Dave", 45)
+player2 = PlayerCharacter("Lily", 16)
 
 print(player1.name)
 print(player2.name)
+
+print(player1.age)
+print(player2.age)
+
 
