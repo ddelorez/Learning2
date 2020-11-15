fd14d548496bf34694de98485e90a34508cb4590
ddelorez <ddelorez@gmail.com>
2020-11-15 16:36:30 -0600

    OOP test

---
+++ main.py
@@ -0,0 +1,15 @@
+#OOP
+
+class PlayerCharacter:
+    def __init__(self, name):
+        self.name = name
+        
+
+    def run(self):
+        print("run")
+
+
+player1 = PlayerCharacter("Dave")
+
+print(player1)
+
